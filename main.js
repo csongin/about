1a14e6082f5bfc4b29e6b053ef50b97a528240f1
Csongin <cheonsong37@gmail.com>
2022-08-11 17:46:56 +0900

    New
    버튼 클릭시 명함 뒤편이 보이는 기능 추가

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ toggleBtn.addEventListener('click', () => {
 });
 
 
+
+function changeDisplay() {
+  const icon = document.querySelector('#home_back');
+
+  if(icon.style.display === 'none') {
+    icon.style.display = 'inline-flex';
+  } else {
+    icon.style.display = 'none';
+  }
+}

664473c36290ab179838ece6f8e7021288c8fc1f
Csongin <cheonsong37@gmail.com>
2022-08-25 10:22:28 +0900

    자기소개 페이지 제출

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-const toggleBtn = document.querySelector('.navbar__toggleBtn');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
-
-toggleBtn.addEventListener('click', () => {
-  menu.classList.toggle('active');
-  icons.classList.toggle('active');
-});
-
-
 
 function changeDisplay() {
   const icon = document.querySelector('#home_back');

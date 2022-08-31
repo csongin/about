8d75c76837c8164dcd77d9b96bd4521444dfe59d
csongin <cheonsong37@gmail.com>
2022-08-31 14:06:14 +0900

    d-day 기능 구현

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ function changeDisplay() {
     iconFront.style.display = 'inline-flex';
   }
 }
+
+let dDay = new Date("January 21, 2022, 0:00:00").getTime();
+
+setInterval(function() {
+  let today = new Date().getTime();
+  let gap =  today - dDay;
+  let day = Math.ceil(gap / (1000 * 60 * 60 * 24));
+  let hour = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let min = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60));
+  let sec = Math.ceil((gap % (1000 * 60)) / 1000);
+
+  document.getElementById("count").innerHTML = `프로그래밍 공부 시작한지 ${day}일 ${hour}시간 ${min}분 ${sec}초 `;
+}, 1000);
